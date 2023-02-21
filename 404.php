b39d3dea28d20e8c6224c2853179821a6ced1ecc
DavidJ97 <98352831+DavidJ97@users.noreply.github.com>
2023-02-21 12:26:28 -0500

    ajout de la page 404

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+    page dans le cas ou y a un probeleme de chemain
+*/
+get_header() ?>
+<main>
+    <code>404.php</code>
+    <h1>Erreur 404 - la page n'existe pas</h1>
+</main> 
+<?php get_footer(); ?>
+
